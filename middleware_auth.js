addbb8725988cdd4b6065e7f859f33d04e076492
kabitaeminence <98816015+kabitaeminence@users.noreply.github.com>
2022-02-24 16:34:51 +0530

    Update middleware_auth.js

--- middleware_auth.js
+++ middleware_auth.js
@@ -3,9 +3,12 @@ const jwt = require('jsonwebtoken');
 const authenticateToken = async (req, res, next) => {
 
     const token = req.headers['authorization']
+//     console.log(req.headers)
     
     try{
        const user = jwt.verify(token, "hhh");
+        
+//         console.log(user)
        req.params.userId = user._id;
     
        next();
