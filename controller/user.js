891af644a17412488eedfde66bff36fbdfeabbdf
kabitaeminence <98816015+kabitaeminence@users.noreply.github.com>
2022-02-24 16:31:57 +0530

    Update user.js

--- controller/user.js
+++ controller/user.js
@@ -1,16 +1,77 @@
 const userData = require("../model/user")
 
-const create1 = async (req, res) => {
-    const {  name, email, password } = req.body;
-    const obj = {
-        name,
-        email,
-        password
+// const create1 = async (req, res) => {
+//     const {  name, email, password } = req.body;
+//     const obj = {
+//         name,
+//         email,
+//         password
+//     }
+//     const user = await userData.create(obj);
+//     user.save()
+//     res.send(user)
+// }
+
+
+const signup = async (req, res) => {
+  try {
+    const pass = await bcrypt.hash(req.body.password, 10);
+    const datastoring = {
+      Name: req.body.Name,
+      email: req.body.email,
+      // number: req.body.number,
+      // password: req.body.password
+      password: pass
+    };
+    const data = await new userData(datastoring);
+    //   console.log(data);
+
+    const result = await userData.findOne({ email: data.email });
+    if (result) {
+      res.send("Email is alreaday taken  take another email or login ");
     }
-    const user = await userData.create(obj);
-    user.save()
-    res.send(user)
-}
+
+    data.save();
+    console.log(data)
+    return res
+      .status(200)
+      .send({ status: 200, message: `sign up has suceesfully welcome ${datastoring.Name}` });
+  } catch (err) {
+    console.log("error", err);
+    return res.status(500).send({ status: 500, message: "Something went wrong" });
+  }
+};
+
+
+const login = async (req, res) => {
+  const password = req.body.password;
+  const data = await userData.findOne({ email: req.body.email });
+  // console.log(data,"__________")
+
+  if (data) {
+    // console.log(data)
+    const check_password = await bcrypt.compare(password, data.password);
+    // console.log(check_password, "((((((((")
+    if (check_password) {
+      // const hash = "hhh";
+      const token = jwt.sign({ _id: data._id }, "hash");
+
+      console.log(data);
+      console.log(token)
+
+      // res.send(token);
+      return res
+        .status(200)
+        .send({ status: true, message: `login successful ${data.Name} `, token: token });
+    } else {
+      return res.send("incorrect password");
+    }
+  } else {
+    return res.status(404).send({ status: false, message: "User Not exist " });
+  }
+};
+
+
 
 const get = async (req, res) => {
     try {
@@ -20,6 +81,18 @@ const get = async (req, res) => {
         console.log(error)
     }
 }
+
+// get data by id-------------------------
+const getById = async(req,res)=>{
+    try{
+        const _id = req.params.id;
+        const getMen = await userData.findById(_id)
+        res.send(getMen);
+    }catch(e){
+        res.status(400).send(e)
+    }
+}
+
 
 const patchD = async (req, res) => {
     try {
