3c61389636205d7e6d4719883cb4e65cba81f461
kabitaeminence <98816015+kabitaeminence@users.noreply.github.com>
2022-02-24 16:33:32 +0530

    Update user.js

--- router/user.js
+++ router/user.js
@@ -1,12 +1,19 @@
 const ControllerUser = require('../controller/user')
 
+const {authenticateToken}= require('../middlewares/auth');
+
 const express = require("express");
 
 const router = express.Router();
 
 router.post("/", ControllerUser.create1);
 
-router.get("/",ControllerUser.get);
+// router.get("/",ControllerUser.get);
+
+router.get("/:id",authenticateToken,ControllerUser.getById);
+
+
+router.get("/",authenticateToken,ControllerUser.get);
 
 router.patch("/:id",ControllerUser.patchD);
 
