5a74cc8c559fe804d395d91baa099cc4dac1c435
kabitaeminence <98816015+kabitaeminence@users.noreply.github.com>
2022-03-14 10:42:17 +0530

    Update userOrder.js

--- controller/userOrder.js
+++ controller/userOrder.js
@@ -41,6 +41,46 @@ const get = async (req, res) => {
 // }
 
 
+const filterdata = async (req, res) => {
+    const data = req.body;
+
+    let query = {};
+
+    let limit = req.body.limit || 10;
+    let skip = req.body.skip || 1;
+    
+    let addressObj = {}
+    if (data.OrderQuentity) query.OrderQuentity = { $gt: data.OrderQuentity }
+    if (data.OrderPrice) query.OrderPrice = { $gt: data.OrderPrice }
+    if (data.state) addressObj.state = { $in: data.state }
+
+    const count =await UserOrder.countDocuments(query)
+
+
+    const filterData = await UserOrder.find(query).populate({path:"addressId",match:addressObj}).limit(limit).skip((skip-1) * limit)
+    console.log(filterData)
+    res.json ({data:filterData,totalCount:count})
+    }
+
+
+
+const search = async (req, res) => {
+    //onst { page } = req.params;
+    const { finnOrderName } = req.params;
+    //const page=req.params.page
+    //const numbers = await users.count();
+    try {
+        const data = await UserOrder.find({
+            $or: [{ OrderName: { $regex: String(finnOrderName), $options: "i" } }],
+        });
+        console.log(data);
+        res.send(data);
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+
 
 
 
